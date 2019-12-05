31fab7c3ccf710aa9661c2e7d853d87473590e14
teckleck <teckleck98@gmail.com>
2019-12-06 02:14:13 +0800

    Added notification in Staff
    Table for new Transaction History

--- app/src/main/java/com/example/login/staff/CheckStock.java
+++ app/src/main/java/com/example/login/staff/CheckStock.java
@@ -1,34 +1,38 @@
 package com.example.login.staff;
 
+<<<<<<< Updated upstream
 import android.content.Intent;
+=======
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.login.R;
+import android.content.Intent;
+import android.graphics.Color;
+>>>>>>> Stashed changes
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.TextView;
 import android.view.ViewGroup;
 import android.graphics.Color;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.login.R;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-//import com.google.firebase.database.R;
-import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.*;
 import com.levitnudi.legacytableview.LegacyTableView;
+
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 import static com.levitnudi.legacytableview.LegacyTableView.MAASAI;
+import static com.levitnudi.legacytableview.LegacyTableView.readLegacyContent;
 
 
 public class CheckStock extends AppCompatActivity {
@@ -37,7 +41,15 @@ public class CheckStock extends AppCompatActivity {
     public static final FirebaseDatabase fablabStock = FirebaseDatabase.getInstance("https://fablabstock.firebaseio.com/");
     public static final DatabaseReference totalStock = fablabStock.getReference("totalStock");
     public static DatabaseReference transactionHistory = fablabStock.getReference("transactionHistory");
+<<<<<<< Updated upstream
     public static int dateSelected;
+=======
+
+    //date selected for date dropdown
+    public static int dateSelected;
+
+    //intent KEYS
+>>>>>>> Stashed changes
     public static final String LGcontent = "lgcontent";
     public static final String LGtitle = "lgtitle";
 
@@ -49,12 +61,22 @@ public class CheckStock extends AppCompatActivity {
         Intent intentTo = getIntent();
         System.out.println();
 
+<<<<<<< Updated upstream
+=======
+        // get Values after search button
+>>>>>>> Stashed changes
         String[] LegacyTitle = intentTo.getStringArrayExtra(CheckStock.LGtitle);
         String[] LegacyContent = intentTo.getStringArrayExtra(CheckStock.LGcontent);
 
 
+<<<<<<< Updated upstream
         if (LegacyContent!=null) {
             System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXX");
+=======
+
+        // only after search, LegacyConent!=null, so build table
+        if (LegacyContent!=null) {
+>>>>>>> Stashed changes
             LegacyTableView.insertLegacyTitle(LegacyTitle);
             LegacyTableView.insertLegacyContent(LegacyContent);
 
@@ -62,8 +84,14 @@ public class CheckStock extends AppCompatActivity {
             legacyTableView.setContent(LegacyTableView.readLegacyContent());
             legacyTableView.setTitle(LegacyTableView.readLegacyTitle());
 
+<<<<<<< Updated upstream
             legacyTableView.setTablePadding(7);
 
+=======
+
+
+            legacyTableView.setTablePadding(7);
+>>>>>>> Stashed changes
             //to enable users to zoom in and out:
             legacyTableView.setZoomEnabled(true);
             legacyTableView.setShowZoomControls(false);
@@ -73,6 +101,11 @@ public class CheckStock extends AppCompatActivity {
             legacyTableView.build();
         }
 
+<<<<<<< Updated upstream
+=======
+
+
+>>>>>>> Stashed changes
         final Spinner items = findViewById(R.id.items);
         final Spinner date = findViewById(R.id.date);
         final List<String> itemSpinner = new ArrayList<>();
@@ -94,6 +127,40 @@ public class CheckStock extends AppCompatActivity {
         dateSpinner.add("Dec");
 
         ArrayAdapter<String> dateAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, dateSpinner){
+<<<<<<< Updated upstream
+=======
+
+            @Override
+            public boolean isEnabled(int position){
+                if(position == 0)
+                {
+                    // Disable the first item from Spinner
+                    // First item will be use for hint
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            @Override
+            public View getDropDownView(int position, View convertView,
+                                        ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if(position == 0){
+                    // Set the hint text color gray
+                    tv.setTextColor(Color.GRAY);
+                }
+                else {
+                    tv.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+        };
+
+        date.setAdapter(dateAdapter);
+>>>>>>> Stashed changes
 
             @Override
             public boolean isEnabled(int position){
@@ -169,7 +236,12 @@ public class CheckStock extends AppCompatActivity {
                 }
             }
             @Override
+<<<<<<< Updated upstream
             public View getDropDownView(int position, View convertView, ViewGroup parent) {
+=======
+            public View getDropDownView(int position, View convertView,
+                                        ViewGroup parent) {
+>>>>>>> Stashed changes
                 View view = super.getDropDownView(position, convertView, parent);
                 TextView tv = (TextView) view;
                 if(position == 0){
@@ -185,9 +257,10 @@ public class CheckStock extends AppCompatActivity {
 
 
         items.setAdapter(itemAdapter);
-        LegacyTableView.insertLegacyTitle("Date","Name","Student ID","Item","Quantity","Purpose","Pillar");
+        LegacyTableView.insertLegacyTitle("Date (YYMMDD)","Student ID","Item","Quantity","Purpose");
 
         items.setOnItemSelectedListener(
+<<<<<<< Updated upstream
             new AdapterView.OnItemSelectedListener() {
                 public void onItemSelected(
                         AdapterView<?> parent, View view, final int position, long id) {
@@ -257,6 +330,83 @@ public class CheckStock extends AppCompatActivity {
                                 System.out.println("CANCELLED");
                             }
                         });
+=======
+                new AdapterView.OnItemSelectedListener() {
+                    public void onItemSelected(
+                            AdapterView<?> parent, View view, final int position, long id) {
+                        //Toast.makeText(CheckStock.this,"Selected Item: " + items.getItemAtPosition(position)
+                        //+ " id=" + id,Toast.LENGTH_LONG).show();
+                        if (position > 0) {
+                            transactionHistory.addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                    for (DataSnapshot d : dataSnapshot.getChildren()) {
+                                        String[] value = new String[5];
+                                        String item ="";
+                                        HashMap<String, Object> hmap = (HashMap) d.getValue();
+                                        if (hmap.get("item").toString().length()==7){
+                                            item += "Transistor";
+                                        }
+                                        else if (hmap.get("item").toString().length()==6){
+                                            item += "Resistor";
+                                        }
+                                        else if (hmap.get("item").toString().length()<=5){
+                                            item += "Capacitor";
+                                        }
+                                        //System.out.println(d.getKey().substring(4,6));
+                                        //System.out.println(d.getKey().substring(4,7));
+                                        //System.out.println(dateSelected);
+                                        System.out.println(hmap.get("YYMMDD").toString().substring(2,4));
+
+                                        if (item.equals(items.getItemAtPosition(position))
+                                                && hmap.get("YYMMDD").toString().substring(2,4).equals(Integer.toString(dateSelected-1))) {
+                                            value[0] = hmap.get("YYMMDD").toString();
+                                            value[1] = hmap.get("studentId").toString();
+                                            value[2] = item + ": " + hmap.get("item").toString();
+                                            value[3] = hmap.get("quantity").toString();
+                                            value[4] = hmap.get("purpose").toString();
+
+                                        } else if (items.getItemAtPosition(position).equals("All")
+                                                && hmap.get("YYMMDD").toString().substring(2,4).equals(Integer.toString(dateSelected-1))) {
+                                            value[0] = hmap.get("YYMMDD").toString();
+                                            value[1] = hmap.get("studentId").toString();
+                                            value[2] = item + ": " + hmap.get("item").toString();
+                                            value[3] = hmap.get("quantity").toString();
+                                            value[4] = hmap.get("purpose").toString();
+
+                                        } else if (items.getItemAtPosition(position).equals("All")
+                                                && dateSelected==1) {
+                                            value[0] = hmap.get("YYMMDD").toString();
+                                            value[1] = hmap.get("studentId").toString();
+                                            value[2] = item + ": " + hmap.get("item").toString();
+                                            value[3] = hmap.get("quantity").toString();
+                                            value[4] = hmap.get("purpose").toString();
+                                        }
+
+                                        if (item.equals(items.getItemAtPosition(position))
+                                                && dateSelected==1) {
+                                            value[0] = hmap.get("YYMMDD").toString();
+                                            value[1] = hmap.get("studentId").toString();
+                                            value[2] = item + ": " + hmap.get("item").toString();
+                                            value[3] = hmap.get("quantity").toString();
+                                            value[4] = hmap.get("purpose").toString();
+                                        }
+
+
+                                        if (value[0] != null) {
+                                            LegacyTableView.insertLegacyContent(value);
+                                        }
+
+                                    };
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError databaseError) {
+                                    System.out.println("CANCELLED");
+                                }
+                            });
+                        }
+>>>>>>> Stashed changes
                     }
                 }
                 public void onNothingSelected(AdapterView<?> parent) {
--- app/src/main/java/com/example/login/staff/Staff.java
+++ app/src/main/java/com/example/login/staff/Staff.java
@@ -1,10 +1,30 @@
 package com.example.login.staff;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.example.login.R;
+<<<<<<< Updated upstream
 import androidx.appcompat.app.AppCompatActivity;
+=======
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+>>>>>>> Stashed changes
 
 import android.view.View;
 import android.widget.Button;
@@ -35,18 +55,46 @@ public class Staff extends AppCompatActivity {
     Button CheckStock;
     Button Chart;
 
+<<<<<<< Updated upstream
     public static final FirebaseDatabase database = FirebaseDatabase.getInstance("https://fablabstock.firebaseio.com/");
     public static final DatabaseReference fablabstockRef = database.getReference("fablabstock");
 
     public static final String CHANNEL_ID = "channel";
     private NotificationManagerCompat notificationManager;
+=======
+    //firebase references
+    public static final FirebaseDatabase database = FirebaseDatabase.getInstance("https://fablabstock.firebaseio.com/");
+    public static final DatabaseReference threshold = database.getReference("Threshold");
+    public static final DatabaseReference totalStockRef = database.getReference("totalStock");
+
+    //notification initialization
+    public static final String CHANNEL_ID = "channel";
+    private NotificationManagerCompat notificationManager;
+    private String notificationContent = "";
+>>>>>>> Stashed changes
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_staff);
 
+<<<<<<< Updated upstream
         notificationManager = NotificationManagerCompat.from(this);
+=======
+        //create notiication channel
+        createNotificationchannel();
+        notificationManager = NotificationManagerCompat.from(this);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+>>>>>>> Stashed changes
 
         //IDS
         //Header = findViewById(R.id.header);
@@ -72,6 +120,7 @@ public class Staff extends AppCompatActivity {
             }
         });
 
+<<<<<<< Updated upstream
         //Notification channel
         createNotificationchannel();
 
@@ -99,11 +148,97 @@ public class Staff extends AppCompatActivity {
                     notificationManager.notify(1, notification);
                 }
             }
+=======
+
+        //NOTIFY STAFF IF ITEM FALLS BELOW THRESHOLD
+        //It goes through 2 datasnapshot : Threshold and totalStock
+        threshold.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                final DataSnapshot thresholdd = dataSnapshot;
+
+
+                totalStockRef.addValueEventListener(new ValueEventListener() {
+
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        for (DataSnapshot item : dataSnapshot.getChildren()) {
+                            for (DataSnapshot id : item.getChildren()) {
+
+
+
+                                // if quantity of item in totalstock is lower than in threshold (int)
+                                if ( (int) (long) thresholdd.child(item.getKey().toString()).getValue()  > (int) (long) id.getValue()) {
+
+                                    notificationContent += item.getKey() + " " + id.getKey() + " is low on stock: " + id.getValue() + "\n";
+
+
+                                }
+
+                                // if notificationConent is not Empty ( there are some items that are below threshold )
+                                if (!notificationContent.isEmpty()) {
+                                    System.out.println(notificationContent);
+
+                                    String notificationTitle = "LOW STOCK";
+                                    Notification notification = new NotificationCompat.Builder(Staff.this, CHANNEL_ID)
+                                            .setSmallIcon(R.drawable.ic_warning_black_24dp)
+                                            .setContentTitle(notificationTitle)
+                                            .setContentText(notificationContent)
+                                            .setStyle(new NotificationCompat.BigTextStyle()
+                                                    .bigText(notificationContent))
+                                            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                                            .build();
+
+                                    notificationManager.notify(1, notification);
+                                }
+
+                            }
+
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        System.out.println("CANCELLED");
+
+                    }
+
+                });
+
+
+
+
+            }
+
+>>>>>>> Stashed changes
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
                 System.out.println("CANCELLED");
             }
         });
+<<<<<<< Updated upstream
+=======
+    }
+
+
+    //method to create a channel for notification
+    public void createNotificationchannel(){
+
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "channel", importance);
+            channel.setDescription("this is notification");
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+>>>>>>> Stashed changes
     }
 
     public void createNotificationchannel(){
