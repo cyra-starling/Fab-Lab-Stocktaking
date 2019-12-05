b6c197391cb1905e0fc7853242642fcd4080236c
zackteo <zackteo@gmail.com>
2019-12-05 21:28:38 +0800

    Pushed Teck Leck's notification changes.

--- app/src/main/java/com/example/login/staff/GenerateChart.java
+++ app/src/main/java/com/example/login/staff/GenerateChart.java
@@ -1,9 +1,26 @@
 package com.example.login.staff;
 
 import com.example.login.R;
+
+import java.util.Calendar;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+
+import android.os.Bundle;
+
+import android.view.Menu;
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -38,40 +55,13 @@ public class GenerateChart extends AppCompatActivity {
 
 
     public static final FirebaseDatabase database = FirebaseDatabase.getInstance("https://fablabstock.firebaseio.com/");
-    public static final DatabaseReference transactionHistoryTestRef = database.getReference("transactionHistoryTest");
+    public static final DatabaseReference transactionHistoryRef = database.getReference("transactionHistory");
 //    public static final DatabaseReference totalStock = database.getReference("totalStock");
-
-    /*
-    //method to get Number of Specified Item taken
-    public int getNumberOfItemTaken(final String item){
-        final List<Long> numbers = new ArrayList<>();
-        transactionHistory.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for (DataSnapshot transactions : dataSnapshot.getChildren()) {
-                    String c = transactions.child("item").getValue().toString();
-                    if (c.equals(item)) {
-                        System.out.println(transactions.child("item").getValue().toString() +" "+ transactions.child("quantity").getValue());
-                        numbers.add((Long) transactions.child("quantity").getValue());
-                    }
-                    System.out.println("???");
-                    //System.out.println("...");
-                }
-            }
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                System.out.println("CANCELLED");
-            }
-        });
-        System.out.println("GETTIN NUMBERR");
-        int result = 0;
-
-        for (Long i : numbers){
-            result+=i;
-        };
-        return result;
-    }
-    */
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private TextView dateView1;
+    private TextView dateView2;
+    private int year, month, day;
 
     private static <K extends Comparable, V extends Comparable> Map<K, V> getSortedMapByValues(final Map<K, V> map){
 
@@ -95,22 +85,56 @@ public class GenerateChart extends AppCompatActivity {
         return mapSortedByValues;
     }
 
+
+    public void generateChart(List<DataEntry> data) {
+        final AnyChartView anyChartView = (AnyChartView) findViewById(R.id.any_chart_view);
+        final Cartesian barChart = AnyChart.bar();
+        Bar column = barChart.bar(data);
+        column.tooltip()
+                .titleFormat("{%X}")
+                .position(Position.CENTER_BOTTOM)
+                .anchor(Anchor.CENTER_BOTTOM)
+                .offsetX(0d)
+                .offsetY(5d)
+                .format("{%Value}{groupsSeparator: }");
+        barChart.barGroupsPadding(0);
+        barChart.animation(false);
+        barChart.title("Amount of Item Taken ");
+        barChart.yScale().minimum(0d);
+        barChart.yAxis(0).labels().format("{%Value}{groupsSeparator: }");
+        barChart.tooltip().positionMode(TooltipPositionMode.POINT);
+        barChart.interactivity().hoverMode(HoverMode.BY_X);
+        barChart.xAxis(0).title("Items");
+        barChart.yAxis(0).title("Number");
+        anyChartView.setChart(barChart);
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generate_chart);
 
-        final AnyChartView anyChartView = (AnyChartView) findViewById(R.id.any_chart_view);
-        Button buildChart = findViewById(R.id.BuildChart);
+        Button refresh = findViewById(R.id.refresh);
 
-        final Cartesian barChart = AnyChart.bar();
+        dateView1 = (TextView) findViewById(R.id.dateSelected1);
+        dateView2 = (TextView) findViewById(R.id.dateSelected2);
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+//        showDate(year, month+1, day);
+
+
         final List<DataEntry> data = new ArrayList<>();
 
 
+        final ProgressBar spinner;
+        spinner = findViewById(R.id.progressBar3);
+        spinner.setVisibility(View.VISIBLE);
 
-
-//        totalStock.addListenerForSingleValueEvent(new ValueEventListener() {
-        transactionHistoryTestRef.orderByChild("date").startAt(1911).endAt(1914).addListenerForSingleValueEvent(new ValueEventListener() {
+        transactionHistoryRef.orderByChild("YYMM").startAt(191101).endAt(191231).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 HashMap itemList = new HashMap<String, Integer>();
@@ -127,41 +151,52 @@ public class GenerateChart extends AppCompatActivity {
                         itemList.put(currentItem, itemQuantity);
                     }
                 }
-
                 // loop through newly created Hash Map
                 Iterator<Entry<String, Integer>> it = getSortedMapByValues(itemList).entrySet().iterator();
                 while (it.hasNext()) {
                     Entry<String, Integer> pair = (Entry<String, Integer>) it.next();
                     data.add(new ValueDataEntry(pair.getKey(), (int)(long)pair.getValue()));
                 }
+                spinner.setVisibility(View.GONE);
+
+                generateChart(data);
             }
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
             }
         });
-
-        buildChart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Bar column = barChart.bar(data);
-                column.tooltip()
-                        .titleFormat("{%X}")
-                        .position(Position.CENTER_BOTTOM)
-                        .anchor(Anchor.CENTER_BOTTOM)
-                        .offsetX(0d)
-                        .offsetY(5d)
-                        .format("{%Value}{groupsSeparator: }");
-                barChart.barGroupsPadding(0);
-                barChart.animation(false);
-                barChart.title("Amount of Item Taken ");
-                barChart.yScale().minimum(0d);
-                barChart.yAxis(0).labels().format("{%Value}{groupsSeparator: }");
-                barChart.tooltip().positionMode(TooltipPositionMode.POINT);
-                barChart.interactivity().hoverMode(HoverMode.BY_X);
-                barChart.xAxis(0).title("Items");
-                barChart.yAxis(0).title("Number");
-                anyChartView.setChart(barChart);
-            }
-        });
     }
+
+
+    @SuppressWarnings("deprecation")
+    public void setDate1(View view) {
+        showDialog(999);
+    }
+
+    @SuppressWarnings("deprecation")
+    public void setDate2(View view) {
+        showDialog(999);
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if (id == 999) {
+            return new DatePickerDialog(this, myDateListener, year, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
+                    // year, month, day
+                    showDate(arg1, arg2+1, arg3);
+                }
+            };
+
+    private void showDate(int year, int month, int day) {
+        dateView1.setText(new StringBuilder().append(day).append("/")
+                .append(month).append("/").append(year));
+    }
+
 }
--- app/src/main/java/com/example/login/staff/Staff.java
+++ app/src/main/java/com/example/login/staff/Staff.java
@@ -4,36 +4,50 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.login.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class Staff extends AppCompatActivity {
+
     TextView Header;
     Button CheckStock;
     Button Chart;
+
+
+    public static final FirebaseDatabase database = FirebaseDatabase.getInstance("https://fablabstock.firebaseio.com/");
+    public static final DatabaseReference fablabstockRef = database.getReference("fablabstock");
+
+    public static final String CHANNEL_ID = "channel";
+    private NotificationManagerCompat notificationManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_staff);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_main);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        notificationManager = NotificationManagerCompat.from(this);
 
         //IDS
         Header = findViewById(R.id.header);
@@ -58,6 +72,53 @@ public class Staff extends AppCompatActivity {
                 startActivity(toRestock);
             }
         });
+
+        //Notification channel
+        createNotificationchannel();
+
+        fablabstockRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String notificationContent = "";
+                for (DataSnapshot item : dataSnapshot.child("totalStock").getChildren()) {
+                    for (DataSnapshot id : item.getChildren()) {
+                        // if quantity of item in totalstock is lower than in threshold
+                        if ((int) dataSnapshot.child("threshold").child(item.getKey()).getValue() > (int) id.getValue()) {
+
+                            notificationContent += item.getKey() + " " + id.getKey() + "is low on stock: " + id.getValue() + "\n";
+                        }
+                    }
+                }
+                if (!notificationContent.isEmpty()) {
+                    String notificationTitle = "LOW STOCK";
+                    Notification notification = new NotificationCompat.Builder(Staff.this, CHANNEL_ID)
+                            .setSmallIcon(R.drawable.ic_android_black_24dp)
+                            .setContentTitle(notificationTitle)
+                            .setContentText(notificationContent)
+                            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                            .build();
+                    notificationManager.notify(1, notification);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                System.out.println("CANCELLED");
+            }
+        });
     }
 
+    public void createNotificationchannel(){
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "channel", importance);
+            channel.setDescription("this is notification");
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
 }
+
